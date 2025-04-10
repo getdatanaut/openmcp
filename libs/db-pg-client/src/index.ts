cd39ab5b9ae02669dbc5e0d64b7fafb0fae857e0
Marc MacLeod <marbemac+gh@gmail.com>
2025-04-10 14:59:41 -0500

    scaffold out pg helper libs
    Signed-off-by: Marc MacLeod <marbemac+gh@gmail.com>

---
+++ libs/db-pg-client/src/index.ts
@@ -0,0 +1,3 @@
+export { initClient } from './init-client.ts';
+export { safeBulkOp } from './safe-bulk.ts';
+export type { InitClientOpts, PgClientMetrics } from './types.ts';
---
+++ libs/db-pg-client/src/types.ts
@@ -0,0 +1,48 @@
+import type { Kysely } from 'kysely';
+import type { Sql } from 'postgres';
+
+interface BaseInitClientOpts {
+  debug?: boolean;
+
+  /**
+   * Re-use existing client, if one was created previously.
+   * Only applicable in the edge client, and usually you will not want this.
+   */
+  reuse?: boolean;
+}
+
+export interface InitClientOptsWithUri extends BaseInitClientOpts {
+  uri: string;
+
+  /**
+   * Max connections, for the clients that support it.
+   */
+  max?: number;
+}
+
+export interface InitClientOptsWithSql extends BaseInitClientOpts {
+  sql: Sql;
+}
+
+export type InitClientOpts = InitClientOptsWithUri | InitClientOptsWithSql;
+
+export interface PgClientMetrics {
+  reset(): void;
+  query: number;
+  error: number;
+  kind: {
+    select: number;
+    insert: number;
+    update: number;
+    delete: number;
+    other: number;
+  };
+}
+
+export interface PgClient<T> {
+  db: Kysely<T>;
+  sql: Sql;
+  metrics: PgClientMetrics;
+}
+
+export type InitClientFn = <T>(opts: InitClientOpts) => PgClient<T>;
