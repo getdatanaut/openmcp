482facef8eb6df5c29ce9872c151082fce8faad0
Jakub Rożek <jakub@datanaut.ai>
2025-04-11 17:35:16 +0200

    feat(remix): extend mcp Server

--- packages/remix/src/server.ts
+++ packages/remix/src/server.ts
@@ -1,34 +1,69 @@
 import { Server } from '@modelcontextprotocol/sdk/server/index.js';
-import { CallToolRequestSchema, ListToolsRequestSchema } from '@modelcontextprotocol/sdk/types.js';
+import {
+  CallToolRequestSchema,
+  type ImplementationSchema,
+  ListToolsRequestSchema,
+} from '@modelcontextprotocol/sdk/types.js';
 import { createMcpManager, type McpManager } from '@openmcp/manager';
+import type { z } from 'zod';
 
 import type { Config } from './config/index.ts';
 import registerClientServers from './register-client-servers.ts';
 import registerServers from './register-servers.ts';
 import { parseToolName } from './utils/tools.ts';
 
-export default async function createRemixServer(config: Config): Promise<Server> {
+class RemixServer extends Server {
+  #manager: McpManager;
+
+  constructor(impl: z.infer<typeof ImplementationSchema>, manager: McpManager) {
+    super(impl, {
+      capabilities: {
+        tools: {},
+      },
+    });
+
+    this.#manager = manager;
+  }
+
+  override async close(): Promise<void> {
+    await Promise.allSettled([this.#manager.close(), await super.close()]);
+  }
+}
+
+export default async function createRemixServer(
+  impl: z.infer<typeof ImplementationSchema>,
+  config: Config,
+): Promise<Server> {
   const manager = createMcpManager({
     id: String(Date.now()),
   });
 
   await registerServers(manager, config);
   await registerClientServers(manager, config);
-
-  const server = new Server(
-    {
-      name: 'openmcp-remix',
-      version: '0.0.0',
-    },
-    {
-      capabilities: {
-        tools: {},
-      },
-    },
-  );
+  const server = new RemixServer(impl, manager);
 
   registerListToolsHandler(server, manager);
+  registerCallToolHandler(server, manager);
 
+  return server;
+}
+
+function registerListToolsHandler(server: Server, manager: McpManager) {
+  server.setRequestHandler(ListToolsRequestSchema, async () => {
+    // todo: should we acquire a mutex lock?
+    try {
+      const tools = Object.values(await manager.clientServers.tools({ lazyConnect: true })).flat();
+      return { tools };
+    } catch (error) {
+      return {
+        content: [{ type: 'text', text: `Error: ${String(error)}` }],
+        isError: true,
+      };
+    }
+  });
+}
+
+function registerCallToolHandler(server: Server, manager: McpManager) {
   server.setRequestHandler(CallToolRequestSchema, async request => {
     const { name, arguments: input } = request.params;
     let serverId: string;
@@ -57,22 +92,5 @@ export default async function createRemixServer(config: Config): Promise<Server>
     }
 
     return res;
-  });
-
-  return server;
-}
-
-function registerListToolsHandler(server: Server, manager: McpManager) {
-  server.setRequestHandler(ListToolsRequestSchema, async () => {
-    // todo: should we acquire a mutex lock?
-    try {
-      const tools = Object.values(await manager.clientServers.tools({ lazyConnect: true })).flat();
-      return { tools };
-    } catch (error) {
-      return {
-        content: [{ type: 'text', text: `Error: ${String(error)}` }],
-        isError: true,
-      };
-    }
   });
 }
