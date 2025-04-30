1fda21b8c76a69512ecc916d7b2e39b1092574a4
Jakub Rożek <jakub@datanaut.ai>
2025-04-30 22:08:21 +0200

    chore(cli): add api export point

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,2 +1,4 @@
+export { default as install } from './commands/install/index.ts';
+export { default as uninstall } from './commands/uninstall/index.ts';
 export { default as run } from './commands/run/index.ts';
 export { default as upload } from './commands/upload/index.ts';
