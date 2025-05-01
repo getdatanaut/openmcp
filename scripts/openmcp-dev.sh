71229c04655fa8c78f48c87736ac8b4c90c6f563
Jakub Rożek <jakub@datanaut.ai>
2025-05-01 20:15:14 +0200

    chore: forward arguments to openmcp script

--- scripts/openmcp-dev.sh
+++ scripts/openmcp-dev.sh
@@ -2,4 +2,4 @@ NODE_ENV=development \
 NODE_OPTIONS="--conditions=development --loader=ts-node/esm" \
 TS_NODE_PROJECT="$(pwd)/packages/cli/tsconfig.json" \
 TS_NODE_TRANSPILE_ONLY=1 \
-openmcp
+openmcp "$@"
