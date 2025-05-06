05d59245b18591f1f56707fd7a619fa75f60e921
Jakub Rożek <jakub@datanaut.ai>
2025-05-06 13:49:30 +0200

    chore(cli): setup import/no-extraneous-dependencies rule

--- packages/cli/eslint.config.js
+++ packages/cli/eslint.config.js
@@ -7,7 +7,8 @@ export default [
     files: ['src/**/*.ts'],
     rules: {
       'no-console': 'error',
-      'import/extensions': 'off' // not needed if moduleResolution is set to node16
+      'import/extensions': 'off', // not needed if moduleResolution is set to node16
+      'import/no-extraneous-dependencies': 'error',
     },
   },
 ];
