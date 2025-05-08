d6a1e9ed53f1be961e15a08e2e4459dac4015d14
Jakub Rożek <jakub@datanaut.ai>
2025-05-08 20:17:45 +0200

    chore(cli): hide uninstall for now

--- packages/cli/src/commands/uninstall/index.ts
+++ packages/cli/src/commands/uninstall/index.ts
@@ -7,7 +7,7 @@ import { builder } from '../install/index.ts';
 import handler from './handler.ts';
 
 export default {
-  describe: 'Uninstall the target',
+  describe: process.env['NODE_ENV'] === 'development' ? 'Uninstall the target' : false,
   command: 'uninstall <target>',
   builder,
   handler: createHandler(async args => {
